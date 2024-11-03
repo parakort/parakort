65e6a965ada9caf04d198668c58b8c02dfcc07dc
Peter <63479070+peterb2396@users.noreply.github.com>
2024-11-03 08:54:29 -0500

    social link ui done

--- source_code/mobile/Components/SocialModal.js
+++ source_code/mobile/Components/SocialModal.js
@@ -1,41 +1,133 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Linking, StyleSheet, Image } from 'react-native';
 import Modal from 'react-native-modal';
+import config from "../app.json"
 
 const SocialModal = ({ isVisible, onClose, provider, updateProfile, username }) => {
   const [text, setText] = useState('');
+  const [url, setUrl] = useState('')
+  const [appUrl, setAppUrl] = useState('')
+  const [icon, setIcon] = useState('')
+
+
 
   // Update text state when username prop changes
   useEffect(() => {
     setText(username);
   }, [username]);
 
+  // Update nav links when user changes profile username
+  useEffect(() => {
+    switch (provider)
+    {
+        case "instagram":
+        {
+            setAppUrl(`instagram://user?username=${text}`)
+            setUrl(`https://www.instagram.com/${text}`)
+            break
+        }
+        case "linkedin":
+        {
+            setAppUrl(`linkedin://in/${text}`)
+            setUrl(`https://www.linkedin.com/in/${text}`)
+            break
+        }
+        case "facebook":
+        {
+            setAppUrl(`fb://profile/${text}`)
+            setUrl(`https://www.facebook.com/${text}`)
+            break
+        }
+    }
+  }, [text])
+
+  // Update icon link based on provider
+  useEffect(() => {
+    switch (provider)
+    {
+        case "instagram":
+        {
+            setIcon(require('../assets/social-icons/instagram.png'))
+            break
+        }
+        case "linkedin":
+        {
+            setIcon(require('../assets/social-icons/linkedin.png'))
+            break
+        }
+        case "facebook":
+        {
+            setIcon(require('../assets/social-icons/facebook.png'))
+            break
+        }
+    }
+  }, [provider])
+
   const handleSubmit = () => {
-    console.log('Entered Text:', text); // Handle the entered text
     updateProfile(`socials.${provider}`, text);
     onClose(); // Close the modal
   };
 
+  const handleLinkTest = async() => {
+    // Test the social link
+    try {
+        const supported = await Linking.canOpenURL(appUrl);
+        await Linking.openURL(supported ? appUrl : url);
+      } catch (error) {
+        console.log(error);
+      }
+  }
+
   return (
-    <Modal isVisible={isVisible} onBackdropPress={onClose}>
+    <Modal isVisible={isVisible} onBackdropPress={() => {setText(username); onClose()}}>
       <View style={styles.modalContent}>
+        <View style = {styles.matContainer}>
+            <Image style={styles.icon} source={icon} />
+        </View>
+
         <Text style={styles.title}>Enter your {provider} username</Text>
         <TextInput
           style={styles.input}
           value={text}
           onChangeText={setText}
           placeholder="Type here..."
-          returnKeyType="done"
-          onSubmitEditing={handleSubmit} // Handle "Enter" key
+          returnKeyType="none"
         />
-        <Button title="Submit" onPress={handleSubmit} />
-        <Button title="Cancel" onPress={onClose} />
+        <View style = {{display: "flex", flexDirection: "row", justifyContent: "space-between", width: "90%"}}>
+            <TouchableOpacity
+                style={{...styles.navButton, backgroundColor: config.app.theme.grey}}
+                onPress={() => handleLinkTest()}
+                >
+                <Text>Test Link</Text>
+            </TouchableOpacity>
+            
+            <TouchableOpacity
+                style={{...styles.navButton, backgroundColor: config.app.theme.blue}}
+                onPress={() => handleSubmit()}
+                >
+                <Text  style={{color: config.app.theme.creme}}>Submit</Text>
+            </TouchableOpacity>
+        </View>
       </View>
     </Modal>
   );
 };
 
 const styles = StyleSheet.create({
+    matContainer: {
+        width: "20%",
+        backgroundColor: "white", 
+        borderRadius: 50, 
+        padding: 10, // Adjust padding to control mat size
+        position: "absolute",
+        top: "-25%"
+    },
+    
+    icon: {
+        width: "100%",
+        height: "100%",
+        aspectRatio: 1,
+    },
   modalContent: {
     backgroundColor: 'white',
     padding: 20,
@@ -51,6 +143,14 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     marginBottom: 10,
     padding: 10,
+  },
+  navButton: {
+    padding: 10,
+    margin: 5,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
   },
 });
 
